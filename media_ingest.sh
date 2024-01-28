2675b5b97d7c4d88f77a8528befcbb195a1f853d
desktop <haydenguyn@gmail.com>
2024-01-27 17:53:36 -0800

    Added logic to check for 2 arguments that are directories

--- media_ingest.sh
+++ media_ingest.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
+# If there is not an input and ouput directory echo usage and exit
 if [ $# -ne 2 ]; then
     echo "Usage: $(basename "$0") <input directory> <output directory>"
     exit 1
 fi
 
-# 1st argument passed will be the input directory otherwise it will be current directory
-input_dir="${1:-.}"
+# If the arguments are directories assign variables, else echo usage and exit
+if [ -d $1 ] && [ -d $2 ]; then
+    # 1st argument passed will be the input directory otherwise it will be current directory
+    input_dir="${1:-.}"
 
-# 2nd argument passed will be the output directory otherwise it will be current directory
-output_dir="${2:-.}"
-
-echo "$input_dir"
-echo "$output_dir"
+    # 2nd argument passed will be the output directory otherwise it will be current directory
+    output_dir="${2:-.}"
+else
+    echo "Usage: $(basename "$0") <input directory> <output directory>"
+    exit 1
+fi
