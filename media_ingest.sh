0c70c4092e5c31b7b82fd1df690d7fe47c71efed
desktop <haydenguyn@gmail.com>
2024-01-28 10:48:05 -0800

    Added function for the file_type choice logic

--- media_ingest.sh
+++ media_ingest.sh
@@ -20,6 +20,34 @@ fi
 
 # filtering: file type or everything, user defined name
 
+file_type() {
+    local PS3="Select a file type: "
+    local file_type=("Video" "Image" "Audio" "Exit")
+
+    select choice in "${file_type[@]}"; do
+        case "$choice" in
+            "Video")
+                echo "video'"
+                break
+                ;;
+            "Image")
+                echo "image'"
+                break
+                ;;
+            "Audio")
+                echo "audio"
+                break
+                ;;            
+            "Exit")
+                exit 1
+                ;;
+            *)
+                echo -e "\nInvalid option. Please enter a number from 1 to ${#file_type[@]}\n"
+                ;;
+            esac
+        done
+}
+
 # User selected filter for all files or specific file types
 PS3="Convert everything or specific file type?"
 file_choice=("All Files" "File Type" "Exit")
@@ -32,7 +60,7 @@ select choice in "${file_choice[@]}"; do
             break
             ;;
         "File Type")
-            # Another filter for the type of file
+            file_type
             break
             ;;
         "Exit")
